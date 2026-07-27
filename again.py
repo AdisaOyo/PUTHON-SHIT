dfc797b6fe2f6a87b8f8b398c7aca13c19cd2295
Splendid <adedayoadewole761@gmail.com>
2026-07-27 16:25:50 +0100

    Delete again.py

--- again.py
+++
@@ -1,9 +0,0 @@
-
-def cookie(x):
-    if type(x) == str:
-        return "Who ate the last cookie? It was Zach!"
-    elif type(x) == int or type(x) == float:
-        return "Who ate the last cookie? It was Monica!"
-       
-print(cookie("hello"))
-
